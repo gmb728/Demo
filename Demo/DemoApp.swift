3242798067d138dcad56c7368e56b192cab9c6ab
gmb728 <gmb728@gmail.com>
2021-05-23 10:48:57 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by SophiaChang on 2021/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
